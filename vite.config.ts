df6be7c43b1ae9d1ca8adeddfd109df860a71e77
Juansec <juanse@example.com>
2026-05-05 15:34:42 -0500

    Fix vite.config.ts syntax

--- vite.config.ts
+++ vite.config.ts
@@ -1,1 +1,17 @@
-base: '/CMM-Liga/',;
+import path from "path"
+import react from "@vitejs/plugin-react"
+import { defineConfig } from "vite"
+import { inspectAttr } from 'kimi-plugin-inspect-react'
+
+export default defineConfig({
+  base: '/CMM-Liga/',
+  plugins: [inspectAttr(), react()],
+  server: {
+    port: 3000,
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+})
